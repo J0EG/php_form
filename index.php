109337457ebadcfc74da8610c5c37a04fa159714
Joe Giordano <joey.giordano@gmail.com>
2023-10-09 12:37:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 
-// Prompt the user for their name.
-$name = readline('Please enter your name: ');
+// Get the user's name from the form data.
+$name = $_POST['name'];
 
-// Get the client's IP address.
-$ip = $_SERVER['REMOTE_ADDR'];
-
-// Get the current date and time.
-$dateTime = date('Y-m-d H:i:s');
-
-// Display the results.
-echo "Name: $name\n";
-echo "IP address: $ip\n";
-echo "Date and time: $dateTime\n";
+// Echo the user's name back to the user.
+echo "Hello, $name!";
 
 ?>
